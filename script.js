e25d5a94521c138af4349e8cd9c5ec4a25056651
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-10-12 19:59:31 +0330

    object

--- script.js
+++ script.js
@@ -72,17 +72,57 @@
 
 
 //array
-const arr =[1, 2, 3,];
-arr.push(10);
-arr.pop(10);
-arr.includes(2);
-arr.unshift(10);
-arr.shift(10);
-arr.reverse();
-arr.indexOf(3);
-arr.slice(1, 3);
-arr.splice(1, 3);
-console.log(arr);
+// const arr =[1, 2, 3,];
+// arr.push(10);
+// arr.pop(10);
+// arr.includes(2);
+// arr.unshift(10);
+// arr.shift(10);
+// arr.reverse();
+// arr.indexOf(3);
+// arr.slice(1, 3);
+// arr.splice(1, 3);
+// console.log(arr);
 
 // listing & array in to in
+
+
+//object
+// const persen = {
+//     name: 'alex',
+//     age: 25,
+//     address: {
+//         city: 'test',
+//     },
+//     nambers: [1, 2, 3, 4]
+// };
+// x = persen.age;
+// x = persen.address.city;
+// x = persen;
+// x = persen.nambers[1];
+// persen.name = 'powel';
+// delete persen.age;
+// console.log(persen);
+// console.log(x);
+
+//access to arry by object
+// const user1 = {
+//     name: 'ali',
+//     age: 25,
+//     addres: {
+//         city: {
+//             id: 1,
+//             name: 'test',
+//         }
+//     }
+// }
+
+// let x;
+// x = Object.keys(user1);
+// x = Object.keys(user1.addres);
+// x = Object.values(user1);
+// x = Object.entries(user1);
+// x = Object.hasOwnerProperty('name');
+// console.log(x);
+
 
