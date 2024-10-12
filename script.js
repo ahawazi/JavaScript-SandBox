c26460d7747418bb6ec81a807f13e82f9e0a9ed3
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-10-12 16:04:38 +0330

    add console options

---
+++ script.js
@@ -0,0 +1,13 @@
+alert('heeeeeey');
+
+console.log('hahaha');
+
+console.error('hahaha');
+
+console.warn('hahaha');
+
+console.group('name of grop');
+console.log('hahaha');
+console.error('hahaha');
+console.warn('hahaha');
+console.groupEnd();
