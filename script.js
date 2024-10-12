df84c5071c41b007900815c056dc5029689bb6be
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-10-12 17:39:13 +0330

    add variables

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-alert('heeeeeey');
+// alert('heeeeeey');
 
-console.log('hahaha');
+// console:
+// console.log('hahaha');
 
-console.error('hahaha');
+// console.error('hahaha');
 
-console.warn('hahaha');
+// console.warn('hahaha');
 
-console.group('name of grop');
-console.log('hahaha');
-console.error('hahaha');
-console.warn('hahaha');
-console.groupEnd();
+// console.group('name of grop');
+// console.log('hahaha');
+// console.error('hahaha');
+// console.warn('hahaha');
+// console.groupEnd();
+
+// Variables:
+// let or other & name = '';
+
+// let firstName = 'ali';
+// let lastName = 'sheri';
+// let age = 25;
+// console.log(firstName, lastName, age);
+
+// var age = 5;
+// age = 6;
+// console.log(age);
+
+// let sal = 5;
+// sal = 6;
+// console.log(sal);
+
+// we can't change the const variable
+// const mah = 5;
+// mah = 6;
+// console.log(mah);
+
+// const roz;
+// roz = 6;
+// console.log(roz);
+
