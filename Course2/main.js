64e1903220e9be59380f082c2d0b80c21912cc82
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-10-17 14:50:22 +0330

    variables, array, math, object, function

--- Course2/main.js
+++ Course2/main.js
@@ -1,1 +1,85 @@
-alert('hooooooooooooooooooo');
+// alert('hooooooooooooooooooo');
+
+//change the text in the html page:
+// document.getElementById('content').innerHTML="welcome";
+
+//show the text in the end of html page:
+// document.write("heloo there");
+
+
+//variables
+//var , let: we can change , const: this is not chenged
+
+//datatype
+//primitive datatype
+//string, number(can be float), boolean, null(object), undifined
+
+
+//math
+// ++ one increaze
+// -- one decreaze
+
+
+//array
+// let number = new Array(1, 2, 3, 'c#', true);
+// let number2 = [4, 5, 6, 'c#', true];
+// console.log(number, number2);
+
+//seee the item in array
+// console.log(number[1]);
+
+//for shange the item
+// Number[1]= 'css';
+
+//push the end of the array
+// Number.push('tailwind');
+
+//push the first of the array:
+// Number.unshift('c++');
+
+//delete the from array
+// Number.pop();
+
+
+//object
+// const student = {
+//     name: 'ali',
+//     lastname: 'shiri',
+//     courses: [
+//         'flan',
+//         'folan',
+//         23,
+//         true
+//     ],
+//     adress: {
+//         country: 'onja',
+//         city: 'inja',
+//     }
+// }
+// console.log(student);
+
+// //add to object:
+// student.email = "example@ex.com"
+// console.log(student);
+
+
+
+//function
+// function log() {
+//     console.log("hahah");
+// }
+// log();
+
+// const log2 = () => {
+//     console.log("bye")
+// }
+// log2();
+
+// //parametr
+// function math(x, y) {
+//     return x*y
+// }
+// //argoument
+// let resoult = math(100, 4);
+// console.log(resoult);
+
